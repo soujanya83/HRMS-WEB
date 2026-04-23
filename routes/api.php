bf6a7d26086aff5c3b14b90ff5beb28f66a52062
Adil-Developer1 <developer1@fraxxratech.com>
2026-04-23 15:37:51 +0530

    color code fixed

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,7 @@ use App\Http\Controllers\API\V1\XeroLeaveApplicationController;
 use App\Http\Controllers\API\V1\EmployeeInvitesEmailcontroller;
 use App\Http\Controllers\API\V1\InterviewQuestionController;
 use App\Http\Controllers\API\V1\ApplicantAnswerController;
-use App\Http\Controllers\API\UserColorController;
+use App\Http\Controllers\API\V1\UserColorController;
 
 use App\Http\Controllers\API\V1\{
     RoleController,
@@ -85,9 +85,9 @@ Route::prefix('v1')->group(function () {
 
     // User color preferences (sidebar/background)
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user/getcolors', [\App\Http\Controllers\Api\UserColorController::class, 'getColors']);
-        Route::post('/user/setcolors', [\App\Http\Controllers\Api\UserColorController::class, 'setColors']);
-        Route::get('/user/allcolors', [\App\Http\Controllers\Api\UserColorController::class, 'allColors']);
+        Route::get('/user/getcolors', [UserColorController::class, 'getColors']);
+        Route::post('/user/setcolors', [UserColorController::class, 'setColors']);
+        Route::get('/user/allcolors', [UserColorController::class, 'allColors']);
     });
 
     Route::post('/send-invite', [EmployeeInvitesEmailcontroller::class, 'sendInvite']);
