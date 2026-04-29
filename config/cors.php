8b0a4863dcac26835743c34e08296fcc10894d7c
tabrej-the-developer <tabrezk294@gmail.com>
2026-04-29 13:59:52 +0530

    cors issue fixed

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://chrispp.com',
+        'https://chrispp.au',
         'http://localhost:5173',      
         'http://localhost:3000',  
         'http://127.0.0.1:5173',  
