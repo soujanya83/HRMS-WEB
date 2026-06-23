16279ccb32107bbcf9ccbb749875421f655dc2f5
developer 1 <developer1@fraxxratech.com>
2026-06-23 11:09:32 +0530

    upload doc

--- config/services.php
+++ config/services.php
@@ -28,6 +28,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'ap-southeast-2'),
     ],
 
+    'aws' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'ap-southeast-2'),
+    ],
+
+    'deepseek' => [
+        'api_key' => env('DEEPSEEK_API_KEY'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
